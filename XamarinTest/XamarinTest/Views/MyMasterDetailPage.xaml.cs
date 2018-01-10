1569bc879b775772e403bd9ae55d566ac710cd6d
Fatih BAYTAR <matanist@hotmail.com>
2018-01-10 20:39:04 +0300

    Add project files.

---
+++ XamarinTest/XamarinTest/Views/MyMasterDetailPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinTest.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MyMasterDetailPage : MasterDetailPage
+	{
+		public MyMasterDetailPage ()
+		{
+			InitializeComponent ();
+            Detail=new NavigationPage(new MyDetailPage());
+            Master=new MyMasterPage();
+		}
+	}
+}
